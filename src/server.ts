97f1c1919d075268f85c41857ac0c5ad458461f9
WesleyPizetta <pizetta45@hotmail.com>
2021-02-23 23:40:32 -0300

    Codigo da aula 2 do nlw

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
+import "reflect-metadata";
 import express from 'express';
-
+import './database'
+import { router } from "./routes";
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica (só uma informação, por exemplo uma imagem)
- */
-
-// 1 parametro => Rota (recurso)
-// 2 parametro => Request/Response
-
- // http://localhost:3333/users
-app.get("/", (request, response) => {
-    return response.json({user: "Wes", age: 21, email: "wesleypizetta@hotmail.com", message: "E aí, beleza?"})
-});
-
-app.post("/", (req, res) => {
-    // *dados recebidos para salvar
-    return res.json({message: "Os dados do usuário foram salvos com sucesso!"})
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server rodando!"));
