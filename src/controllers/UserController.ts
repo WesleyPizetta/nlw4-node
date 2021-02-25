2acedcec50749cb0472dfab56ef7e4b839e31d01
WesleyPizetta <pizetta45@hotmail.com>
2021-02-25 08:27:25 -0300

    primeira parte da aula 3

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import { request, Request, response, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(req: Request, res: Response) {
         const {name, email} = req.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -29,4 +29,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
